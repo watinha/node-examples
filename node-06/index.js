87cbc7ec55a16b4a80c6f22e32d34c0dbc9e82e4
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2018-07-11 20:07:16 -0300

    updating package.json to implement start build script

--- node-06/index.js
+++ node-06/index.js
@@ -5,8 +5,9 @@ var http = require('http'),
 server = http.createServer(function (req, res) {
     let file = req.url === '/' ? 'static/index.html' : './static' + req.url;
     fs.readFile(file, function (err, data) {
-        if (err) {
+        if (err && err !== null) {
             console.log('error in file reading...');
+            console.log(err);
             return ;
         }
         res.write(data);
