d4cdeb1e4045e7535c1ea18266e549a4f2e6910b
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2018-08-09 20:46:11 -0300

    more javascript node.js examples

--- node-00/index.js
+++ node-00/index.js
@@ -31,3 +31,73 @@ console.log(pessoa.profissao.local);
 console.log(pessoa.profissao.disciplinas[1]);
 console.log(pessoa.campo);
 console.log(pessoa.campo.campo);
+
+for (var i = 0; i < 10; i++) {
+    console.log(i + " exemplo");
+};
+
+let i = 5;
+while (i < 10 && i > 3) {
+    i++;
+}
+
+let sup = true, i = 0;
+do {
+    if (i++ > 10)
+        sup = !sup;
+} while (sup);
+
+var a = 20, b = 30;
+if (a < b) {
+    console.log("legal")
+} else {
+    console.log("não legal");
+}
+
+var a = 2;
+switch (a) {
+    case 2:
+        console.log("opa 2");
+        break;
+    case 3:
+        console.log("quase 3");
+        break;
+    case 4:
+        console.log("já deu...");
+        break;
+    default:
+        console.log("tentativa");
+}
+
+function somar (a, b) {
+    return a + b;
+}
+somar(20 + 3);
+
+
+console.log(1 == true); // true
+console.log(1 === true); // false
+console.log("0" == false); // true
+console.log("abc" == "a" + "b" + "c"); // true
+console.log(null == undefined); // true
+console.log(30 - "7"); // 23
+console.log("30" + 7); // 307
+
+
+function retangulo (l, a) {
+    this.largura = l;
+    this.altura = a;
+}
+retangulo.prototype.area = function () {
+    return this.largura * this.altura;
+}
+let r = new retangulo(10, 2);
+console.log(r.area());
+
+
+
+
+
+
+
+
