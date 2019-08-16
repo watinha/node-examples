cc1e6ba02f9640bd171ed969b4a3e09f7236a2ee
Willian Watanabe <watinha@gmail.com>
2019-08-16 14:50:51 -0300

    multer upload example with images

--- express-03-upload/index.js
+++ express-03-upload/index.js
@@ -1,1 +1,25 @@
-let fs = require('fs');
+let http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    app = express(),
+    multer = require('multer'),
+    upload = multer({ dest: 'public/uploads' }),
+    images = [];
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'view'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    console.log(images);
+    res.render('index.hbs', { images: images });
+});
+
+app.post('/upload', upload.single('arquivo'), (req, res) => {
+    images.push(req.file.filename)
+    res.redirect('/');
+});
+
+
+
+app.listen(3000);
