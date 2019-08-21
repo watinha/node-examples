fa97483777ef75ae98e8a97e960ab08e5f2a5a6a
Willian Watanabe <watinha@gmail.com>
2019-08-21 18:42:04 -0300

    configuration changes

--- express-heroku-mongodb/index.js
+++ express-heroku-mongodb/index.js
@@ -3,14 +3,16 @@ let http = require('http'),
     express = require('express'),
     app = express(),
     user_controller = require('./app/controller/user'),
-    user_model = require('./app/model/user');
+    user_model = require('./app/model/user'),
+    mongodb_uri = process.env.MONGODB_URI || 'mongodb://mongo:27017/mongo-test',
+    port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app/view'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-user_model.config('mongodb://mongo:27017/mongo-test');
+user_model.config(mongodb_uri);
 
 app.use('/user', user_controller);
 
-app.listen(3000);
+app.listen(port);
